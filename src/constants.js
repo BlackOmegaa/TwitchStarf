de91766cd39ac23aa6d10a1be433f5a48ecbaf51
BlackOmegaa <64774685+BlackOmegaa@users.noreply.github.com>
2021-04-28 14:13:16 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CHANNEL_NAME = '#BIackOmega';
+export const CHANNEL_NAME = '#RoYeZeBoulet';
 export const OAUTH_TOKEN = 'oauth:cp8ncmblxw45f06ikrqqzbkar5x6aq';
 export const BOT_USERNAME = 'StarfouBot';
 export const BLOCKED_WORDS = [
