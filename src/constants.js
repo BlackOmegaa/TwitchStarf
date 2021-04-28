8a3c8fd7ad8168b762ad4f4d44d2edc5bb6f76bd
BlackOmegaa <64774685+BlackOmegaa@users.noreply.github.com>
2021-04-28 14:01:18 +0200

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const CHANNEL_NAME = '#BIackOmega';
+export const OAUTH_TOKEN = 'oauth:cp8ncmblxw45f06ikrqqzbkar5x6aq';
+export const BOT_USERNAME = 'StarfouBot';
+export const BLOCKED_WORDS = [
+  'negre',
+  'nigger',
+  'fdp',
+  'bite',
+]
